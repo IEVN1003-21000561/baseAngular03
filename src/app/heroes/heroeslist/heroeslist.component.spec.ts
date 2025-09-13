51d2e0d3ef009ec4473e2c24250002b385f0221f
Alexandra Flores <21000561@alumnos.utleon.edu.mx>
2025-09-12 18:43:34 -0600

    baseAngular03

---
+++ src/app/heroes/heroeslist/heroeslist.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeroeslistComponent } from './heroeslist.component';
+
+describe('HeroeslistComponent', () => {
+  let component: HeroeslistComponent;
+  let fixture: ComponentFixture<HeroeslistComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HeroeslistComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HeroeslistComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/heroes/heroeslist/heroeslist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroeslist',
+  standalone: false,
+  templateUrl: './heroeslist.component.html',
+  styleUrl: './heroeslist.component.css'
+})
+export class HeroeslistComponent {
+
+}
