ebb981262dfb3f61515ac9e3e76371fe82a03a72
Alexandra Flores <21000561@alumnos.utleon.edu.mx>
2025-09-17 19:36:38 -0600

    Formularios

--- src/app/heroes/heroeslist/heroeslist.component.ts
+++ src/app/heroes/heroeslist/heroeslist.component.ts
@@ -4,8 +4,38 @@ import { Component } from '@angular/core';
   selector: 'app-heroeslist',
   standalone: false,
   templateUrl: './heroeslist.component.html',
-  styleUrl: './heroeslist.component.css'
+  //styleUrl: './heroeslist.component.css'
 })
 export class HeroeslistComponent {
 
+  imageWidth:number=40;
+  imageMargin:number=2;
+  showImage:boolean=true;
+  listFilter:string='';
+
+  showImage():void{
+    this.muestraImage=!this.muestraImage;
+  }
+
+
+  export class app-heroeslist{
+
+    heroes:any[]=[
+    {
+      imagen:"https://dragonball-api.com/transformaciones/vegeta mega instinto.webp",
+      nombre:"vegeta",
+      descripcion:"Maikankosapo y Flis",
+      race:"Saiyan",
+      ki:9500
+    },
+    {
+      imagen:"https://dragonball-api.com/planetas/Tierra_Dragon_Ball_Z.webp"
+      nombre:"Bulma",
+      descripcion:"Ninguno",
+      race:"Humana",
+      ki:0
+    }
+    ]
+  }
 }
+
